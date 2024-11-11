637f03d278510a43a604cb0de1afb34f0e019ff8
rezuan98 <rezuanahmmeds@gmail.com>
2024-11-11 18:49:55 +0600

    responsiveness is acheived for homepage using media query

--- resources/views/frontend/parts/category-6.blade.php
+++ resources/views/frontend/parts/category-6.blade.php
@@ -1,20 +1,20 @@
-<div class="grid-container">
-    <div class="item1"><a href="{{ route('product.details') }}"><img src="{{ asset('/product_images/p7.jpg') }}" alt="Product 2" class="product-image">
+<div class="grid-container-6">
+    <div class="itemnum-1"><a href="{{ route('product.details') }}"><img src="{{ asset('/product_images/p7.jpg') }}" alt="Product 2" class="product-image">
         <span class="category_name">category-6</span></a></div> <!-- Spans 2 columns and 2 rows -->
-    <div class="item2"><div class="item1"><a href="{{ route('product.details') }}"><img src="{{ asset('/product_images/p7.jpg') }}" alt="Product 2" class="product-image">
+    <div class="itemnum-2"><div ><a href="{{ route('product.details') }}"><img src="{{ asset('/product_images/p7.jpg') }}" alt="Product 2" class="product-image">
         <span class="category_name">Full Sleeve tee-shirt</span></a></div> <!-- Spans 2 columns and 2 rows --></div>
-    <div class="item3"><div class="item1"><a href="{{ route('product.details') }}"><img src="{{ asset('/product_images/p7.jpg') }}" alt="Product 2" class="product-image">
+    <div class="itemnum-3"><div ><a href="{{ route('product.details') }}"><img src="{{ asset('/product_images/p7.jpg') }}" alt="Product 2" class="product-image">
         <span class="category_name">Full Sleeve tee-shirt</span></a></div> <!-- Spans 2 columns and 2 rows --></div>
-    <div class="item4"><div class="item1"><a href="{{ route('product.details') }}"><img src="{{ asset('/product_images/p7.jpg') }}" alt="Product 2" class="product-image">
+    <div class="itemnum-4"><div ><a href="{{ route('product.details') }}"><img src="{{ asset('/product_images/p7.jpg') }}" alt="Product 2" class="product-image">
         <span class="category_name">Full Sleeve tee-shirt</span></a></div> <!-- Spans 2 columns and 2 rows --></div>
-    <div class="item5"><div class="item1"><a href="{{ route('product.details') }}"><img src="{{ asset('/product_images/p7.jpg') }}" alt="Product 2" class="product-image">
+    <div class="itemnum-5"><div ><a href="{{ route('product.details') }}"><img src="{{ asset('/product_images/p7.jpg') }}" alt="Product 2" class="product-image">
         <span class="category_name">Full Sleeve tee-shirt</span></a></div> <!-- Spans 2 columns and 2 rows --></div>
-    <div class="item6"><div class="item1"><a href="{{ route('product.details') }}"><img src="{{ asset('/product_images/p7.jpg') }}" alt="Product 2" class="product-image">
+    <div class="itemnum-6"><div ><a href="{{ route('product.details') }}"><img src="{{ asset('/product_images/p7.jpg') }}" alt="Product 2" class="product-image">
         <span class="category_name">Full Sleeve tee-shirt</span></a></div> <!-- Spans 2 columns and 2 rows --></div>
-    <div class="item7"><div class="item1"><a href="{{ route('product.details') }}"><img src="{{ asset('/product_images/p7.jpg') }}" alt="Product 2" class="product-image">
+    <div class="itemnum-7"><div ><a href="{{ route('product.details') }}"><img src="{{ asset('/product_images/p7.jpg') }}" alt="Product 2" class="product-image">
         <span class="category_name">Full Sleeve tee-shirt</span></a></div> <!-- Spans 2 columns and 2 rows --></div>
-    <div class="item8"><div class="item1"><a href="{{ route('product.details') }}"><img src="{{ asset('/product_images/p7.jpg') }}" alt="Product 2" class="product-image">
+    <div class="itemnum-8"><div ><a href="{{ route('product.details') }}"><img src="{{ asset('/product_images/p7.jpg') }}" alt="Product 2" class="product-image">
         <span class="category_name">Full Sleeve tee-shirt</span></a></div> <!-- Spans 2 columns and 2 rows --></div>
-    <div class="item9"><div class="item1"><a href="{{ route('product.details') }}"><img src="{{ asset('/product_images/p7.jpg') }}" alt="Product 2" class="product-image">
+    <div class="itemnum-9"><div ><a href="{{ route('product.details') }}"><img src="{{ asset('/product_images/p7.jpg') }}" alt="Product 2" class="product-image">
         <span class="category_name">Full Sleeve tee-shirt</span></a></div> <!-- Spans 2 columns and 2 rows --></div>
 </div>
--- resources/views/frontend/parts/testimonial.blade.php
+++ resources/views/frontend/parts/testimonial.blade.php
@@ -1,9 +1,15 @@
 <div class="testimonial-container">
 
-    <h1>Work with us Today
+    <div class="text-testimonial">
+        <h1>Work with us Today
        
         </h1>
         <h2> We are the official merchandising partner of </h2>
         <p>We are proud to work with over a thousand brands and organizations that we call friends.
             As your partner, we value long-term relationships and collaborate toward results.</p>
+    </div>
+
+    <div class="image-testimonial">
+        <img src="{{ asset('product_images/pant3.jpg') }}" alt="">
+    </div>
 </div>
